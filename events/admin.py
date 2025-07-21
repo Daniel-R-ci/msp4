fe4b21afaa244e680da0547cb9b07153647f3d36
Daniel-R-ci <daniel@rydells.nu>
2025-07-21 20:06:57 +0200

    EVENTS - In admin.py, show full name for registrations instead of username

--- events/admin.py
+++ events/admin.py
@@ -12,7 +12,12 @@ class EventAdmin(admin.ModelAdmin):
 
 @admin.register(Event_Registration)
 class Event_Registration_Admin(admin.ModelAdmin):
-    list_display = ('event_title', 'event_time', 'user', 'confirmed', 'created_on') # noqa
+    list_display = ('event_title', 'event_time', 'user_full_name', 'confirmed', 'created_on') # noqa
     readonly_fields = ('created_on',)
     ordering = ['event_time', 'event_title', 'user']
     list_filter = ['event_title', 'event_time']
+
+    def user_full_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+    user_full_name.short_description = 'User'
+    user_full_name.admin_order_field = 'user__first_name' 
